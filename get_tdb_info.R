def6969eadee9604d72c887e18628ce7e6a1e700
Jim Lutz <jdlutz@hotwaterresearch.net>
2017-12-18 18:41:16 -0800

    winnowing out showers that aren't in both *.mdb and *.tdb files. Initial comparison of showers in total water to showers in hot water.

--- get_tdb_info.R
+++ get_tdb_info.R
@@ -18,23 +18,11 @@ l_tdbs <- list.files(path = str_sub(wd_data, 1, -2), # get rid of / at end
                      recursive = TRUE, 
                      full.names = TRUE)
 
-str_extract(l_tdbs,"[0-9]{5}")
-
-get.tdb.info(l_tdbs[23])
-
 # build a data.table from all the *.tdb files
 DT_tdb_info <- data.table(ldply(.data=l_tdbs,
                                 .fun =get.tdb.info,
                                 .progress= "text", 
                                 .inform=TRUE))
-
-# r in fread("table.csv") : File is empty: table.csv
-# Error: with piece 124: 
-# [1] "/home/jiml/HotWaterResearch/projects/hwds/shower_clearing_draw/data/DT_tdb_info.RData"
-# was trying to read DT_tdb_info.RData
-
-DT_tdb_info[,KEYCODE]
-
 
 # save the data.table
 save(DT_tdb_info, file = paste0(wd_data,"DT_tdb_info.RData"))
