425db62070d58ede58d9800f46ad76aa5ca6ba8c
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-03-02 08:51:19 -0800

    got an ugly loop to identify shower intervals in one slkm. sometimes duplicate records with same time in DT_tw_flows

--- collect_shower_intervals.R
+++ collect_shower_intervals.R
@@ -33,58 +33,83 @@ DT_shower_interval4[,list(nrec = sum(DURATION)/10)]
 # want to have the following fields
 # study logging meter KEYCODE  
 
-# look for some sample data with coincident draws
-DT_shower_interval4[study   == 'Seattle'  &
-                    logging == 1          &
-                    KEYCODE == 13197      ,
-                    list(START, END, ncoincid)
-                    ]
-
-# plot some events
-s='Seattle' 
-l=1 
-k=13197 
-DT=DT_shower_interval4 
-t1="1999-10-28 05:14:00"
-t2="1999-10-28 05:30:00"
-save.charts=FALSE
-
-plot_shower(s, l, k, DT=DT_shower_interval4, t1, t2,)
-
-
-p
-
-# try this
+# get shower_intervals for one slk
 s='Seattle' 
 l=2 
 k=13197 
-DT=DT_shower_interval4 
 
-  # s = study               - Seattle | EBMUD
-  # l = logging             - 1 | 2 | 3 , the phase of the study
-  # k = KEYCODE             - 5 digit integer that identifies site
-  # DT=DT_shower_interval4  - information about shower interval data
+# get the filename of the total water interval data
+tw_file <- DT[study==s & KEYCODE==k & logging==l & meter=='total water',list(tdw_file=unique(tdb_file))]
+
+# load the total water Flow data as a data.table
+DT_tw_flows <- get_table(fn_database = tw_file, db_table = 'Flows')
+
+# add identifying fields and remove unneeded fields
+DT_tw_flows[,`:=`(study   = s,
+                  KEYCODE = k,
+                  logging = l,
+                  meter   = 'total water',
+                  ID      = NULL,
+                  EventID = NULL)
+            ]
+
+# get the shower edges
+DT_edges <- DT[tdb_file==tw_file, list(START,END, all=1)]
+# all is a dummy variable for melt
+
+# melt from wide to long
+DT_edges <- melt(DT_edges, 
+                 id.vars = c("all") , measure.vars = c("START","END"),
+                 variable.name = "edge", value.name = "StartTime")
+
+# drop all
+DT_edges[,all:=NULL]
+
+# sort DTs on StartTime
+setkey(DT_edges, StartTime)
+setkey(DT_tw_flows, StartTime)
+
+# merge DT_edges onto DT_tw_flows
+DT_flows <- merge(DT_tw_flows,DT_edges, all=TRUE)
+
+# change missing edge to 'NO'
+DT_flows[is.na(edge), edge:='NO']
+
+# add shower flag
+DT_flows[,shower:=FALSE]
+
+# loop through records one at a time, assume first record is not a shower
+for (i in 2:nrow(DT_flows)){
+  this_edge = DT_flows[i,edge]
+  prev_edge = DT_flows[i-1,edge]
+  prev_shower = DT_flows[i-1,shower]
+
+  if(prev_edge=='END') {
+    DT_flows[i, shower:=FALSE]
+    next()
+  }
+  if(this_edge=='NO') {
+    DT_flows[i, shower:=prev_shower]
+    next()
+  }
   
-# DT_1slk.END is a data.table of just meter START END for  one slk.
-DT_1slk.END <- DT[ study==s & logging == l & KEYCODE == k, list(meter, START, END, ncoincid)]
+  if(this_edge=='START') {
+    DT_flows[i, shower:=TRUE]
+  }
+  
+  if(this_edge=='END') {
+    DT_flows[i, shower:=TRUE]
+  }
+  
+}
 
-# count of showers in total water
-nshowers <- nrow(DT_1slk.END[meter == 'total water'])
+# that was slow!
+# see if it worked
 
-# test the find.closest.hots function
-DT_ENDs <- data.table(ldply(.data=1:nshowers, .fun =find.closest.hots, .progress= "text", .inform=TRUE))
+DT_flows
+DT_flows[shower==TRUE,]
+DT_flows[shower==TRUE,][1:100]
+DT_flows[edge!='NO']
 
-
-# plot some to see what's going on
-t1="2000-03-26 18:21:00"
-t2="2000-03-26 18:50:00"
-save.charts=FALSE
-
-plot_shower(s, l, k, DT=DT_shower_interval4, t1, t2,)
-
-# that one looks like it has a lot of coincident draws
-# Aquacraft sees only 5 coincident draws same total & hot
-# also looks like this showerhead is single lever, temperature select only
-
-
+# sometimes duplicate records with same time in DT_tw_flows?
 
