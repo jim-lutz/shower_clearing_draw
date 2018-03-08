b445526f6815d79eb371b0e35a36d78b3a58e981
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-03-08 08:27:54 -0800

    several of the Flow tables have duplicate StartTimes with different Rates. ?

--- collect_shower_intervals.R
+++ collect_shower_intervals.R
@@ -16,65 +16,99 @@ load(file = paste0(wd_data,"DT_shower_interval4.RData"))
 DT_shower_interval4
 
 # want to have the following fields
-# study logging meter KEYCODE  
+# study KEYCODE logging meter   
 
-# get shower_intervals for one slk
-s='Seattle' 
-l=2 
-k=13197 
+# make sklm identifier
+DT_shower_interval4[ , sklm:= paste(study,KEYCODE,logging,meter, sep = '_')]
 
-# get the filename of the total water interval data
-tw_file <- DT_shower_interval4[study==s & KEYCODE==k & logging==l & meter=='total water',list(tdw_file=unique(tdb_file))]
+# how many
+nrow(DT_shower_interval4[,list(unique(sklm))])
+# [1] 101
 
-# load the total water Flow data as a data.table
-DT_tw_flows <- get_table(fn_database = tw_file, db_table = 'Flows')
+# get shower_intervals for one slkm
+s='EBMUD'; l=3; k=22027; m='total water'
+DT=DT_shower_interval4
 
-# add identifying fields and remove unneeded fields
-DT_tw_flows[,`:=`(study   = s,
-                  KEYCODE = k,
-                  logging = l,
-                  meter   = 'total water',
-                  ID      = NULL,
-                  EventID = NULL)
-            ]
-
-# get the shower edges
-DT_edges <- DT_shower_interval4[tdb_file==tw_file, list(START,END, all=1)]
-# all is a dummy variable for melt
-
-# melt from wide to long
-DT_edges <- melt(DT_edges, 
-                 id.vars = c("all") , measure.vars = c("START","END"),
-                 variable.name = "edge", value.name = "StartTime")
-
-# drop all
-DT_edges[,all:=NULL]
-
-# sort DTs on StartTime
-setkey(DT_edges, StartTime)
-setkey(DT_tw_flows, StartTime)
-
-# merge DT_edges onto DT_tw_flows
-DT_flows <- merge(DT_tw_flows,DT_edges, all=TRUE)
-
-# change missing edge to 'NO'
-DT_flows[is.na(edge), edge:='NO']
-
-# add initial shower flag
-DT_flows[,shower:=FALSE]
-
-# 2nd copy for comparison
-DT_flows2 <- DT_flows
-identical(DT_flows, DT_flows2)
-
-# time the loop
-system.time({
+# make a collect_shower_intervals function
+collect_shower_intervals <- function(s, k, l, m, DT) {
+  # function to collect the shower interval data for one sklm
+  # returns data.table of just the shower intervals
+  # s = study               - Seattle | EBMUD
+  # l = logging             - 1 | 2 | 3 , the phase of the study
+  # k = KEYCODE             - 5 digit integer that identifies site
+  # m = meter               - 'total water'|'hot water'
+  # DT=DT_shower_interval4  - information about shower interval data
   
+  # get the filename of the interval data
+  interval_filename <- DT_shower_interval4[study==s & KEYCODE==k & logging==l & meter==m,list(tdw_file=unique(tdb_file))]
+  
+  # load the Flow data as a data.table
+  DT_Flow <- get_table(fn_database = interval_filename, db_table = 'Flows')
+  
+  # add identifying fields and remove unneeded fields
+  DT_Flow[,`:=`(study   = s,
+                KEYCODE = k,
+                logging = l,
+                meter   = m,
+                ID      = NULL,
+                EventID = NULL)
+              ]
+  
+  # drop records without any Rate data
+  DT_Flow[is.na(Rate)]
+  # these should be dropped
+  
+  # duplicate records with same time in DT_Flow?
+  DT_dup_Flow <- DT_Flow[duplicated(DT_Flow[,list(StartTime)]),list(StartTime)][order(StartTime)]
+  merge(DT_Flow, DT_dup_Flow, all.y = TRUE )
+  # is this a general problem or only some of the slmks?
+  # looked at 3 sklms. all 3 had duplicates
+  
+  # get the shower edges
+  DT_edges <- DT_shower_interval4[tdb_file==interval_filename, list(START,END, all=1)]
+  # all is a dummy variable for melt
+  
+  # melt from wide to long
+  DT_edges <- melt(DT_edges, 
+                   id.vars = c("all") , measure.vars = c("START","END"),
+                   variable.name = "edge", value.name = "StartTime")
+  
+  # drop all
+  DT_edges[,all:=NULL]
+
+  # duplicate records with same time in DT_edges?
+  DT_dup_edges <- DT_edges[duplicated(DT_edges[,list(StartTime)]),list(StartTime)][order(StartTime)]
+  merge(DT_edges, DT_dup_edges, all.y = TRUE )
+  # is this a general problem or only some of the sklms?
+  
+  
+    
+  
+  
+  # sort DTs on StartTime
+  setkey(DT_edges, StartTime)
+  setkey(DT_Flow, StartTime)
+  
+  # merge DT_edges onto DT_tw_flows
+  DT_flows <- merge(DT_Flow,DT_edges, all=TRUE)
+  
+  # change missing edge to 'NO'
+  DT_flows[is.na(edge), edge:='NO']
+  
+  # initialize shower flag
+  DT_flows[,shower:=FALSE]
+  
+  # reorder the columns
+  setcolorder(DT_flows, c('study', 'KEYCODE', 'logging', 'meter', 
+                          'edge', 'shower', 'StartTime', 'Rate'))
+
   # number of records to check
   nrec = nrow(DT_flows)
-  
-  # instead of testing, change all subsequent records as appropriate
-  for (i in 1:nrec){ #  assume first record is not a shower
+    
+  # loop through all the records
+  for (i in 1:nrec){ 
+    # if it's not an edge, go to next record
+    if(DT_flows[i,edge]=='NO') {next()}
     
     # if START, change current and all subsequent records to TRUE
     if(DT_flows[i,edge]=='START') {
@@ -85,46 +119,9 @@ system.time({
     if(DT_flows[i,edge]=='END') {
       DT_flows[(i+1):nrec, shower:=FALSE]
     } 
-    
-  }
   
-})
-#   user  system elapsed 
-# 14.316   0.004  14.319 
-
-
-
-# time the loop
-system.time({
-  
-# number of records to check
-nrec = nrow(DT_flows2)
-
-# change all subsequent records as appropriate
-for (i in 1:nrec){
-  # if not edge, go to next record
-  if(DT_flows2[i,edge]=='NO') {next()}
-  
-  # if START, change current and all subsequent records to TRUE
-  if(DT_flows2[i,edge]=='START') {
-    DT_flows2[i:nrec, shower:=TRUE]
-  } 
-  
-  # if END, change all subsequent records to FALSE
-  if(DT_flows2[i,edge]=='END') {
-    DT_flows2[(i+1):nrec, shower:=FALSE]
   } 
   
 }
 
-})
-#  user  system elapsed 
-# 7.140   0.000   7.137 
-# better, but still slow
-
-identical(DT_flows, DT_flows2)
-# [1] TRUE
-
-# sometimes duplicate records with same time in DT_tw_flows?
-# some StartTimes without any data?
 
