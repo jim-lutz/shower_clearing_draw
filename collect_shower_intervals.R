6e46d46dac9a42d6d1ab3385858a05b208f23e9d
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-03-08 07:11:33 -0800

    reduced time of loop to test shower intervals by almost 50%

--- collect_shower_intervals.R
+++ collect_shower_intervals.R
@@ -39,7 +39,7 @@ l=2
 k=13197 
 
 # get the filename of the total water interval data
-tw_file <- DT[study==s & KEYCODE==k & logging==l & meter=='total water',list(tdw_file=unique(tdb_file))]
+tw_file <- DT_shower_interval4[study==s & KEYCODE==k & logging==l & meter=='total water',list(tdw_file=unique(tdb_file))]
 
 # load the total water Flow data as a data.table
 DT_tw_flows <- get_table(fn_database = tw_file, db_table = 'Flows')
@@ -54,7 +54,7 @@ DT_tw_flows[,`:=`(study   = s,
             ]
 
 # get the shower edges
-DT_edges <- DT[tdb_file==tw_file, list(START,END, all=1)]
+DT_edges <- DT_shower_interval4[tdb_file==tw_file, list(START,END, all=1)]
 # all is a dummy variable for melt
 
 # melt from wide to long
@@ -75,8 +75,15 @@ DT_flows <- merge(DT_tw_flows,DT_edges, all=TRUE)
 # change missing edge to 'NO'
 DT_flows[is.na(edge), edge:='NO']
 
-# add shower flag
+# add initial shower flag
 DT_flows[,shower:=FALSE]
+
+# 2nd copy for comparison
+DT_flows2 <- DT_flows
+identical(DT_flows, DT_flows2)
+
+# time the loop
+system.time({
 
 # loop through records one at a time, assume first record is not a shower
 for (i in 2:nrow(DT_flows)){
@@ -103,13 +110,57 @@ for (i in 2:nrow(DT_flows)){
   
 }
 
+})  
+#   user  system elapsed 
+# 28.316   0.004  28.313 
 # that was slow!
-# see if it worked
 
+# see if it worked
 DT_flows
 DT_flows[shower==TRUE,]
 DT_flows[shower==TRUE,][1:100]
 DT_flows[edge!='NO']
 
 # sometimes duplicate records with same time in DT_tw_flows?
+# some StartTimes without any data?
 
+# time the loop
+system.time({
+  
+# try a different strategy to see if it's any faster
+nrec = nrow(DT_flows2)
+
+# instead of testing, change all subsequent records as appropriate
+for (i in 1:nrec){ #  assume first record is not a shower
+
+  # if START, change current and all subsequent records to TRUE
+  if(DT_flows2[i,edge]=='START') {
+    DT_flows2[i:nrec, shower:=TRUE]
+  } 
+  
+  # if END, change all subsequent records to FALSE
+  if(DT_flows2[i,edge]=='END') {
+    DT_flows2[(i+1):nrec, shower:=FALSE]
+  } 
+  
+}
+
+})
+#   user  system elapsed 
+# 14.256   0.000  14.262 
+# better, but still slow
+
+# see if it worked
+DT_flows2
+DT_flows2[shower==TRUE,]
+DT_flows2[shower==TRUE,][1:100]
+DT_flows2[edge!='NO']
+# seems to have
+
+identical(DT_flows, DT_flows2)
+# [1] TRUE
+
+# how many sklm?
+DT_shower_interval4[!is.na(study), sklm:= paste(study,KEYCODE,logging,meter, sep = '_')]
+nrow(DT_shower_interval4[,list(unique(sklm))])
+# [1] 101
