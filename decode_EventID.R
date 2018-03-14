3871037aacc70011a1c5e28705c60a9212216af8
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-03-13 17:42:46 -0700

    may have figured out how to name flows with 2 merges.

--- decode_EventID.R
+++ decode_EventID.R
@@ -1,6 +1,6 @@
 # decode_EventID.R
-# script to extract shower interval data and save to one .Rdata file for later processing
-# Jim Lutz "Thu Mar  1 09:20:29 2018"
+# script to help decode EventID in Flows data
+# Jim Lutz "Tue Mar 13 16:27:54 2018"
 
 # set packages & etc
 source("setup.R")
@@ -25,7 +25,7 @@ DT_shower_interval4[ , sklm:= paste(study,KEYCODE,logging,meter, sep = '_')]
 nrow(DT_shower_interval4[,list(unique(sklm))])
 # [1] 101
 
-# find a shower with 2 or 3 coincidents
+# find a shower with a few coincidents
 DT_shower_interval4[,list(nshower=length(USETYPE), ncoin=sum(ncoincid)),by=sklm][order(-nshower,-ncoin)][1:30]
 # Seattle_13431_1_total water
 
@@ -48,10 +48,6 @@ DT_shower_interval4[study   == s &
 # 6:        1 1999-11-07 16:25:04
 # 7:        1 1999-11-08 12:56:04
 
-=================
-
-
-
 # get the .tdb filename
 fn_database = DT_shower_interval4[study   == s & 
                                   KEYCODE == k &
@@ -62,7 +58,7 @@ fn_database = DT_shower_interval4[study   == s &
 # call mdb-tables, output to table.csv
 system2("mdb-tables", args = c('-1', paste0("'",fn_database,"'")), stdout = "table.csv")
 
-# load the output from the temporary file
+# load the output from the temporary file as a list
 l_tables <- fread("table.csv", header = FALSE)[[1]]
 
 # delete the temporary table file
@@ -87,14 +83,126 @@ tables()$NAME
 # [7] "DT_VirtualFixtures" 
 
 tables()[,NAME]
+
+# look at Flows
 names(DT_Flows)
 # [1] "ID"        "EventID"   "StartTime" "Rate" 
+summary(DT_Flows$EventID)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+#    1     759    1578    1573    2421    3080
+
+# look at Events
 names(DT_Events)
 # [1] "ID"        "StartTime" "EndTime"   "Duration"  "Class"     "Peak"      "Volume"   
 # [8] "Mode"      "ModeFreq" 
+nrow(DT_Events)
+# [1] 3080
+# same number of Events as number of EventID in Flows
 
-DT_VirtualFixtures[grep('Shower',Name),ID]
-# [1] 4
+# look at EventFixtures
+names(DT_EventFixtures)
+# [1] "ID"        "IDEvent"   "IDFixture" "Preserved"
+length(DT_EventFixtures$IDEvent)
+# [1] 3080
+# same as EventID in Flows
+# every IDEvent has an IDFixture
+# how many IDFixtures
+DT_EventFixtures[, list(nrecs = length(ID)), by=IDFixture][order(IDFixture)]
+ #     IDFixture nrecs
+ #  1:         2  1273
+ #  2:         3   335
+ #  3:         5    35
+ #  4:         6    53
+ #  5:         7    60
+ #  6:         9  1213
+ #  7:        10    40
+ #  8:        12    37
+ #  9:        13     4
+ # 10:        14    28
+ # 11:        16     2
+# what about Preserved
+DT_EventFixtures[, list(nrecs = length(ID)), by=Preserved][order(Preserved)]
+#    Preserved nrecs
+# 1:         0  2784
+# 2:         1   296
+# what's Preserved
+DT_EventFixtures[Preserved==1, 
+                 list(nrecs = length(ID)), 
+                 by=IDFixture][order(IDFixture)]
+#    IDFixture nrecs
+# 1:         3    24
+# 2:         5    15
+# 3:         6     7
+# 4:         7    21
+# 5:         9   122
+# 6:        10    40
+# 7:        12    37
+# 8:        14    28
+# 9:        16     2
+
+identical(DT_EventFixtures$ID,DT_EventFixtures$IDEvent)
+# [1] TRUE
+
+
+# now see about Fixtures
+names(DT_Fixtures)
+# [1] "ID"               "VirtualFixtureID" "Order"            "Name"            
+# [5] "MinVol"           "MaxVol"           "MinPeak"          "MaxPeak"         
+# [9] "MinDur"           "MaxDur"           "MinMode"          "MaxMode"         
+# [13] "MinModeFreq"      "MaxModeFreq"     
+DT_Fixtures
+# this looks like mostly parameters to determine what type of event it is
+# What about VirtualFixtureID?
+# More ID in Fixtures than IDFixture in EventFixtures
+nrow(DT_Fixtures)
+# [1] 14
+
+names(DT_VirtualFixtures)
+[1] "ID"          "Order"       "Name"        "MinVol"      "MaxVol"      "MinPeak"    
+[7] "MaxPeak"     "MinDur"      "MaxDur"      "MinMode"     "MaxMode"     "MinModeFreq"
+[13] "MaxModeFreq"
+DT_VirtualFixtures
+nrow(DT_VirtualFixtures)
+# [1] 13
+
+DT_Fixtures[,list(ID, Name, VirtualFixtureID)]
+DT_VirtualFixtures[,list(ID, Name)]
+
+DT_Fixtures[,mvar:=VirtualFixtureID]
+setkey(DT_Fixtures,mvar)
+DT_VirtualFixtures[,mvar:=ID]
+setkey(DT_VirtualFixtures,mvar)
+
+tables()[NAME %in% c('DT_Fixtures','DT_VirtualFixtures')]
+merge(DT_Fixtures, DT_VirtualFixtures)[,list(mvar, Name.x, Name.y)]
+#     mvar          Name.x        Name.y
+#  1:    1        Toilet 1        Toilet
+#  2:    1        Toilet @        Toilet
+#  3:    2    Dishwasher 1    Dishwasher
+#  4:    2    Dishwasher @    Dishwasher
+#  5:    3 Clotheswasher 1 Clotheswasher
+#  6:    3 Clotheswasher 2 Clotheswasher
+#  7:    3 Clotheswasher @ Clotheswasher
+#  8:    4        Shower 1        Shower
+#  9:    5       Bathtub 1       Bathtub
+# 10:    8          Leak 1          Leak
+# 11:    9    Irrigation 1    Irrigation
+# 12:   10        Faucet 1        Faucet
+# 13:   11         UNKNOWN         Other
+# 14:   12         OUTDOOR    Humidifier
+ 
+# seems like VirtualFixtures is more generic
+# @ is per load? 
+# since 'Shower 1' and 'Shower' are the same, 
+# probably don't have to go all the way to VirtualFixtures
+
+# DT_Flows$
+
+
+
+
+
+
 
 DT_Fixtures[grep('Shower',Name),list(ID,VirtualFixtureID) ]
 #    ID VirtualFixtureID
