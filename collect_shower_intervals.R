1231e90afb70693a111aab40e096cfa475b87503
Jim Lutz <jdlutz@hotwaterresearch.net>
2018-03-15 14:33:18 -0700

    collect.showers as a function to get identified shower Flows data for one sklm

--- collect_shower_intervals.R
+++ collect_shower_intervals.R
@@ -25,23 +25,42 @@ DT_shower_interval4[ , sklm:= paste(study,KEYCODE,logging,meter, sep = '_')]
 nrow(DT_shower_interval4[,list(unique(sklm))])
 # [1] 101
 
-# make
+# make a list of sklm's
+l_sklm <- DT_shower_interval4[,list(sklm=unique(sklm))][order(sklm)][[1]]
+
 # get shower_intervals for one slkm
-s='EBMUD'; l=3; k=22027; m='total water'
-DT=DT_shower_interval4
+l_sklm[27]
 
 
+collect.showers <- function(this_sklm, DT=DT_shower_interval4) {
+  # collects shower Flows data for one sklm
+  # sklm  = string consisting of study_logging_KEYCODE_meter
+  # DT    = data.table DT_shower_interval4 with sklm field already added
+  
+  # recover the sklm's, there's got to be a more elegant way to do this
+  DT_sklm <- DT_shower_interval4[sklm==l_sklm[27]][1,list(study,logging,KEYCODE,meter)]
+  s = DT_sklm$study
+  l = DT_sklm$logging
+  k = DT_sklm$KEYCODE
+  m = DT_sklm$meter
+  
+  # get the Flows as a data.table with Name field added
+  DT_Flows <- get.Names(s, l, k, m, DT)
+  
+  # add identifying fields to every record
+  DT_Flows[,`:=`(study   = s,
+                 KEYCODE = k,
+                 logging = l,
+                 meter   = m)
+           ]
+  
+  # keep only shower records 
+  DT_shower_Flows <- DT_Flows[grep('Shower',Name),]
+  
+  # return the shower Flows data.table
+  return(DT_shower_Flows)
+  
+}
 
-# get the Flows as a data.table with Name field added
-DT_Flows <- get.Names(s, l, k, m, DT)
-
-# add identifying fields to every record
-DT_Flows[,`:=`(study   = s,
-               KEYCODE = k,
-               logging = l,
-               meter   = m)
-         ]
-
-# keep only shower records 
-DT_Flows <- DT_Flows[grep('Shower',Name),]
-# 560 records out of 6254 in this case
+# test the collect.showers function
+DT_shower_Flows <- collect.showers(l_sklm[27],DT_shower_interval4)
